4724b9087169b074c9baff157961f8b22ea0b9b6
EdithClara <161049202+edithclara@users.noreply.github.com>
2024-03-11 19:38:24 +0000

    Add files via upload

--- PCA.py
+++ PCA.py
@@ -5,8 +5,8 @@ from sklearn.preprocessing import StandardScaler
 from matplotlib import pyplot as plt
 
 # loading dataset into the program
-dataset1 = pd.read_csv('D:\SNS\London 2000-01-01 to 2024-01-31.csv', index_col="datetime")
-dataset2 = pd.read_csv(r'D:\SNS\tfl-daily-cycle-hires.csv', index_col="Day", parse_dates=["Day"])
+dataset1 = pd.read_csv('London 2000-01-01 to 2024-01-31.csv', index_col="datetime")
+dataset2 = pd.read_csv(r'tfl-daily-cycle-hires.csv', index_col="Day", parse_dates=["Day"])
 
 # filter dataset1 from 2010.07.30 to 2024.01.31
 start_date = pd.to_datetime('2010-07-30')
@@ -37,13 +37,13 @@ perc_missing_values = merged_data.isnull().sum()/merged_data.shape[0]
 print(perc_missing_values)
 
 # removing columns where null percentage is greater than 10
-columns_cleaned = merged_data.columns[perc_missing_values<.1]
+columns_cleaned = merged_data.columns[perc_missing_values < 0.1]
 
 # changing the dataset so that it only contains required columns
-merged_data=merged_data[columns_cleaned].copy()
+merged_data = merged_data[columns_cleaned].copy()
 
 # Filling in the missing data in columns by the previous day values
-merged_data=merged_data.ffill()
+merged_data = merged_data.ffill()
 
 # Checking again the number of missing data in every column after filling in the missing values
 print(merged_data.isnull().sum())
@@ -57,6 +57,7 @@ print(merged_data.index.year.value_counts().sort_index())
 # print final dataset
 merged_data.drop('name', axis=1, inplace=True)
 final_dataset = merged_data.copy()
+print('Print the final dataset:')
 print(final_dataset.head())
 print(final_dataset.tail())
 print(final_dataset.shape)
