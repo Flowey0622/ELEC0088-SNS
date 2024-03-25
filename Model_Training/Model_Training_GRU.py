2efc91f522293a3927ee020523efc6744f4efbe3
ayse58 <ayseguzel58@gmail.com>
2024-03-25 22:13:35 +0000

    Update Model_Training_GRU.py

--- Model_Training/Model_Training_GRU.py
+++ Model_Training/Model_Training_GRU.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from tensorflow.keras import layers
+from keras import layers
 from keras.models import Sequential
 from keras.optimizers import RMSprop
 from matplotlib import pyplot as plt
